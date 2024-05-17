b170aeebad9fbbe534876a985aaba4708fdc2af2
foshanxiaoyu <1422054416@qq.com>
2024-05-17 09:50:55 +0800

    AnimeCard show

--- anime_brower/next.config.mjs
+++ anime_brower/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{ 
+        remotePatterns:[ // API get 图
+           { 
+            protocol:'https',
+            hostname:'*',
+           },
+           { 
+            protocol:'https',
+            hostname:'foshanxiaoyu.github.io/images',
+           },
+        ] 
+    }
+    
+};
 
 export default nextConfig;
